31783877d7c0cfb6eda05d544aaec9f687d6d144
Bartek Pacia <barpac02@gmail.com>
2023-08-08 08:57:21 +0200

    add PlaygroundUITests.m (to test setUp and tearDown)

--- LandmarksUITests/LandmarksUITests.m
+++ LandmarksUITests/LandmarksUITests.m
@@ -61,3 +61,128 @@
 }
 
 @end
+//@import XCTest;
+//@import ObjectiveC.runtime;
+//
+//@interface ParametrizedTests : XCTestCase
+//@end
+//
+//@implementation ParametrizedTests
+//
+//- (void)testTruthness {
+//  NSLog(@"testTruthness called!");
+//  XCTAssertTrue(true, @"true is true");
+//}
+//
+//+ (NSArray<NSInvocation *> *)testInvocations {
+//  NSLog(@"testInvocations() called");
+//
+//  /* Prepare dummy input */
+//  __block NSMutableArray<NSString *> *dartTestFiles = [[NSMutableArray alloc] init];
+//  [dartTestFiles addObject:@"example_test"];
+//  [dartTestFiles addObject:@"permissions_location_test"];
+//  [dartTestFiles addObject:@"permissions_many_test"];
+//
+//  NSMutableArray<NSInvocation *> *invocations = [[NSMutableArray alloc] init];
+//
+//  NSLog(@"Before the loop, %lu elements in the array", (unsigned long)dartTestFiles.count);
+//
+//
+//  for (int i = 0; i < dartTestFiles.count; i++) {
+//    /* Step 1 */
+//
+//    NSString *name = dartTestFiles[i];
+//
+//    void (^func)(ParametrizedTests *) = ^(ParametrizedTests *instance) {
+//      XCUIApplication* app = [[XCUIApplication alloc] init];
+//      [app launch];
+//      NSLog(@"func called!");
+//      XCTAssertTrue(true, "dummy assert");
+//      [NSRunLoop.currentRunLoop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];
+//    };
+//
+//    IMP implementation = imp_implementationWithBlock(func);
+//    NSString *selectorStr = [NSString stringWithFormat:@"test_%@", name];
+//    SEL selector = NSSelectorFromString(selectorStr);
+//    class_addMethod(self, selector, implementation, "v@:");
+//
+//    /* Step 2 */
+//
+//    NSMethodSignature *signature = [self instanceMethodSignatureForSelector:selector];
+//    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+//    invocation.selector = selector;
+//
+//    NSLog(@"RunnerUITests.testInvocations(): selectorStr = %@", selectorStr);
+//
+//    [invocations addObject:invocation];
+//  }
+//
+//  NSLog(@"After the loop");
+//  
+//  /* add statically-defined testTruthness() test method to testInvocation list */
+//  
+//  NSMethodSignature *signature = [self instanceMethodSignatureForSelector:@selector(testTruthness)];
+//  NSInvocation *trueInvocation = [NSInvocation invocationWithMethodSignature:signature];
+//  trueInvocation.selector = @selector(testTruthness);
+//  
+//  [invocations addObject:trueInvocation];
+//
+//  return invocations;
+//}
+//
+//@end
+//
+//@interface ParametrizedTests2 : XCTestCase
+//@end
+//
+//@implementation ParametrizedTests2
+//+ (NSArray<NSInvocation *> *)testInvocations {
+//  NSLog(@"testInvocations() called");
+//
+//  /* Prepare dummy input */
+//  __block NSMutableArray<NSString *> *dartTestFiles = [[NSMutableArray alloc] init];
+//  [dartTestFiles addObject:@"example_test"];
+//  [dartTestFiles addObject:@"permissions_location_test"];
+//  [dartTestFiles addObject:@"permissions_many_test"];
+//
+//  NSMutableArray<NSInvocation *> *invocations = [[NSMutableArray alloc] init];
+//
+//  NSLog(@"Before the loop, %lu elements in the array", (unsigned long)dartTestFiles.count);
+//
+//
+//  for (int i = 0; i < dartTestFiles.count; i++) {
+//    /* Step 1 */
+//
+//    NSString *name = dartTestFiles[i];
+//
+//    void (^func)(ParametrizedTests *) = ^(ParametrizedTests *instance) {
+//      XCUIApplication* app = [[XCUIApplication alloc] init];
+//      [app launch];
+//      NSLog(@"func called!");
+//      XCTAssertTrue(true, "dummy assert");
+//      [NSRunLoop.currentRunLoop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];
+//    };
+//
+//    IMP implementation = imp_implementationWithBlock(func);
+//    NSString *selectorStr = [NSString stringWithFormat:@"test_%@", name];
+//    SEL selector = NSSelectorFromString(selectorStr);
+//    class_addMethod(self, selector, implementation, "v@:");
+//
+//    /* Step 2 */
+//
+//    NSMethodSignature *signature = [self instanceMethodSignatureForSelector:selector];
+//    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+//    invocation.selector = selector;
+//
+//    NSLog(@"RunnerUITests.testInvocations(): selectorStr = %@", selectorStr);
+//
+//    [invocations addObject:invocation];
+//  }
+//
+//  NSLog(@"After the loop");
+//
+//  return invocations;
+//}
+//
+//@end
+//
---
+++ LandmarksUITests/PlaygroundUITests.m
@@ -0,0 +1,64 @@
+@import XCTest;
+@import ObjectiveC.runtime;
+
+@interface PlagroundAlphaTests : XCTestCase
+@end
+
+@implementation PlagroundAlphaTests
+
++ (void)setUp {
+  NSLog(@"%@: class setUp()", NSStringFromClass([self class]));
+}
+
+- (void)setUp {
+  NSLog(@"%@: instance tearDown()", NSStringFromClass([self class]));
+}
+
+- (void)tearDown {
+  NSLog(@"%@: instance tearDown()", NSStringFromClass([self class]));
+}
+
++ (void)tearDown {
+  NSLog(@"%@: class tearDown()", NSStringFromClass([self class]));
+}
+
+- (void)testAddition {
+  XCTAssertTrue(true, "addition works");
+}
+
+- (void)testSubtraction {
+  XCTAssertTrue(true, "subtraction works");
+}
+
+@end
+
+@interface PlagroundBravoTests : XCTestCase
+@end
+
+@implementation PlagroundBravoTests
+
++ (void)setUp {
+  NSLog(@"%@: class setUp()", NSStringFromClass([self class]));
+}
+
+- (void)setUp {
+  NSLog(@"%@: instance tearDown()", NSStringFromClass([self class]));
+}
+
+- (void)tearDown {
+  NSLog(@"%@: instance tearDown()", NSStringFromClass([self class]));
+}
+
++ (void)tearDown {
+  NSLog(@"%@: class tearDown()", NSStringFromClass([self class]));
+}
+
+- (void)testAddition {
+  XCTAssertTrue(true, "addition works");
+}
+
+- (void)testSubtraction {
+  XCTAssertTrue(true, "subtraction works");
+}
+
+@end
