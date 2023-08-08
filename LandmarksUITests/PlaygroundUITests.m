d61a55137b5560fb8e89ff7d74d540e609efec5f
Bartek Pacia <barpac02@gmail.com>
2023-08-08 15:12:59 +0200

    remove classs PlaygroundBravoTests from PlaygroundUITests.m

--- LandmarksUITests/PlaygroundUITests.m
+++ LandmarksUITests/PlaygroundUITests.m
@@ -41,34 +41,3 @@
 }
 
 @end
-
-@interface PlagroundBravoTests : XCTestCase
-@end
-
-@implementation PlagroundBravoTests
-
-+ (void)setUp {
-  NSLog(@"%@: class setUp()", NSStringFromClass([self class]));
-}
-
-- (void)setUp {
-  NSLog(@"%@: instance setUp()", NSStringFromClass([self class]));
-}
-
-- (void)tearDown {
-  NSLog(@"%@: instance tearDown()", NSStringFromClass([self class]));
-}
-
-+ (void)tearDown {
-  NSLog(@"%@: class tearDown()", NSStringFromClass([self class]));
-}
-
-- (void)testAddition {
-  XCTAssertTrue(true, "addition works");
-}
-
-- (void)testSubtraction {
-  XCTAssertTrue(true, "subtraction works");
-}
-
-@end
